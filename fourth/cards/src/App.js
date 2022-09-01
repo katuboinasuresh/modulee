6d6b1c5c26be9578cc357ecfd75518a46e59baa6
katuboinasuresh <katuboinasuresh@gmail.com>
2022-09-01 13:55:34 +0530

    cards

--- fourth/cards/src/App.js
+++ fourth/cards/src/App.js
@@ -3,44 +3,46 @@ import './App.css';
 
 function App() {
   return (
-    <div classname="con">
-      <div classname="text">
+    <div className="con">
+      <div className="text">
         <h1 id="he" >Reliable,efficient delivery</h1>
         <h1>Powered by Technology</h1>
-        <p>cards document designrd by HTML&CSS with respect display sizes<br>Install the latest PowerShell for new</br>
-        </p>
+        <p>cards document designrd by HTML and CSS with respect display sizes</p>
+        <p>Install the latest PowerShell for new</p>
+
       </div>
-      <div classname="cards">
-        <div classname="c1">
-          <h3 classname="h">Supervisor</h3>
-          <p classname="p">html & css document demo test<br>by 10x academy</br></p>
-          <img classname="i" src="https://tse2.mm.bing.net/th?id=OIP.ihTI_m375yIdcOoj0bhblgHaIR&pid=Api&P=0"
+      <div className="cards">
+        <div className="c1">
+          <h3 className="h">Supervisor</h3>
+          <p className="p">html and css document demo test</p>
+          <img className="i" src="https://tse2.mm.bing.net/th?id=OIP.ihTI_m375yIdcOoj0bhblgHaIR&pid=Api&P=0"
             width="45px" height="45px" alt="ioc"></img>
         </div>
-        <div classname="c2">
-          <div classname="c21">
-            <h3 classname="h">Team Builder</h3>
-            <p classname="p">html & css document demo test<br>by 10x academy</br></p>
-            <img classname="i"
+        <div className="c2">
+          <div className="c21">
+            <h3 className="h">Team Builder</h3>
+            <p className="p">html and css document demo test</p>
+            <img className="i"
               src="https://thumbs.dreamstime.com/b/corporate-team-icon-black-white-background-shadow-vector-illustration-147095194.jpg"
               width="45px" height="45px" alt="ioc"></img>
           </div>
-          <div classname="c22">
-            <h3 classname="h">Karma</h3>
-            <p classname="p">html & css document demo test<br>by 10x academy</br></p>
-            <img classname="i" src="https://tse3.mm.bing.net/th?id=OIP.kE5P_fIe2OrmWmHtzR4BqgHaHa&pid=Api&P=0"
+          <div className="c22">
+            <h3 className="h">Karma</h3>
+            <p className="p">html and css document demo test</p>
+            <img className="i" src="https://tse3.mm.bing.net/th?id=OIP.kE5P_fIe2OrmWmHtzR4BqgHaHa&pid=Api&P=0"
               width="45px " height="45px" alt="ioc"></img>
           </div>
         </div>
-        <div class="c3">
-          <h3 class="h">Calculator</h3>
-          <p class="p">html & css document demo test<br>by 10x academy</br></p>
-          <img class="i" src="https://cdn.onlinewebfonts.com/svg/img_47335.png" width="45px" height="45px"
+        <div className="c3">
+          <h3 className="h">Calculator</h3>
+          <p className="p">html and css document demo test</p>
+
+          <img className="i" src="https://cdn.onlinewebfonts.com/svg/img_47335.png" width="45px" height="45px"
             alt="ioc"></img>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default App;
